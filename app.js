fe233a2a03dbac4feadee86e517c80e870a1491e
binmaocom <binmaocom@gmail.com>
2018-07-03 14:38:21 +0700

    update port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http'),
 socketIO = require('socket.io'),
-port = process.env.PORT || 3001,
+port = process.env.PORT || 8080,
 ip = process.env.IP || '127.0.0.1',
 server = http.createServer().listen(port, ip, function(){
 console.log('Socket.IO server started at %s:%s!', ip, port);
