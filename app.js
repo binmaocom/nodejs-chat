603e916946b3ae142f176b75559a8da4ca7f4c33
binmaocom <binmaocom@gmail.com>
2018-07-03 17:03:22 +0700

    no message

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ socketIO = require('socket.io'),
 port = process.env.PORT || 8080,
 ip = process.env.IP || '127.0.0.1',
 server = http.createServer().listen(port, ip, function(){
-console.log('Socket.IO server started at %s:%s!', ip, port);
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
 });
 io = socketIO.listen(server);
 io.set('match origin protocol', true);
