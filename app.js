f146d3be6713d6778ce94f54d402724c440243c1
binmaocom <binmaocom@gmail.com>
2018-07-03 18:16:09 +0700

    no message

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var http = require('http'),
 request = require('request'),
 socketIO = require('socket.io'),
 port = process.env.PORT || 8080;
-// http.createServer(function (req, res) {
-    // res.writeHead(200, {'Content-Type': 'text/plain'});
-    // res.end('Hello World!');
-// }).listen(8081);
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(port);
 
-var server = http.createServer().listen(port),
+var server = http.createServer().listen(3000),
 io = socketIO.listen(server);
 io.set('match origin protocol', true);
 io.set('origins', '*:*');
