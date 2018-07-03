236e8d9dbccbb166120ad786b295e48d5b1bcb64
binmaocom <binmaocom@gmail.com>
2018-07-03 17:09:29 +0700

    no message

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-// var http = require('http');
-// var port = process.env.PORT || 8080;
 var http = require('http');
-var socketIO = require('socket.io');
-var port = process.env.PORT || 8080;
-var ip = process.env.IP || '127.0.0.1';
-var server = http.createServer().listen(port, ip, function(){
-	res.writeHead(200, {'Content-Type': 'text/plain'});
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!');
-});
+}).listen(8080);
