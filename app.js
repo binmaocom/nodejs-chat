c4569729ce1f85cce604b7f81fbdcf4ff5b039cd
binmaocom <binmaocom@gmail.com>
2018-07-03 18:07:40 +0700

    no message

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ port = process.env.PORT || 8080;
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!');
-}).listen(port);
+}).listen(8081);
 
 var server = http.createServer().listen(port),
 io = socketIO.listen(server);
