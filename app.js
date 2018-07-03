90a297ee61d99b4ed71d4796874ff6b347ef93a8
binmaocom <binmaocom@gmail.com>
2018-07-03 17:49:25 +0700

    no message

--- app.js
+++ app.js
@@ -13,12 +13,3 @@ io.set('origins', '*:*');
 io.set('log level', 1);
 
 const request = require('request');
-const headerstring = {
-	'charset':'utf-8',
-	'Cache-Control':'no-cache',
-	'Connection':'keep-alive',
-	'Cookie':'__utmx=138759908.pT1e8xoVTLOSJ8kCwpYFGA$294858-496:2;',
-	'Pragma':'no-cache',
-	'Upgrade-Insecure-Requests':1,
-	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
-};
