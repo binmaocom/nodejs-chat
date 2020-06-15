a9b030b19cc436fe0942482ec4cb03e6ee782e3c
Viet Bun <binmaocom@users.noreply.github.com>
2020-06-15 12:00:51 +0700

    update

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ const headerstring = {
 var run = function(socket){
 	// Socket process here!!!
 	socket.emit('greeting', 'Hello from Socket.IO');
-	socket.broadcast.to(socket.id).emit('update_socket_id', socket.id);
+	//socket.broadcast.to(socket.id).emit('update_socket_id', socket.id);
+	socket.emit('update_socket_id', socket.id);
 	socket.on('send_to_socket_id', function(data){
 		if (typeof data[1]!=='undefined'){
 			socket.broadcast.to(data[1]).emit('send_to_socket_id', data);	
@@ -52,7 +53,7 @@ var run = function(socket){
 					// console.log(body);
 					socket.emit('get-new-tasks', body);
 					// socket.broadcast.emit('get-new-tasks', body);					
-					socket.emit('send_to_socket_id', socket.id);
+					//socket.emit('send_to_socket_id', socket.id);
 				}
 				else {
 					socket.emit('get-new-tasks', response);
